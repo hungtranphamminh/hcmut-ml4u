1dfcbc40158caa50675dbeb94e0bf6a7917d0156
artemis_h <tranphamminhhung65@gmail.com>
2025-02-14 16:46:33 +0700

    feat(sidebar): add left side decorator bar

---
+++ src/components/sidebar/recruit-bar.tsx
@@ -0,0 +1,14 @@
+export default function LeftBar() {
+  return (
+    <div className="w-[80px] h-screen fixed hidden xl:flex top-[60px] left-0 z-[9999] bg-white items-center pl-4 font-geist text-sm text-blue-500">
+      <div
+        className=""
+        style={{
+          writingMode: "sideways-lr",
+        }}
+      >
+        RECRUIT 2025
+      </div>
+    </div>
+  );
+}
