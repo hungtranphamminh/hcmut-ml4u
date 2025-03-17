d7ef2a1488e5515cf151d5412edd96b931881abc
artemis_h <tranphamminhhung65@gmail.com>
2025-03-17 16:45:52 +0700

    feat: add dockerfile for local dev

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,13 @@ const nextConfig: NextConfig = {
   basePath: process.env.NODE_ENV === "production" ? "/hungtranghamminh.github.io" : "",
   assetPrefix: process.env.NODE_ENV === "production" ? "/hungtranghamminh.github.io" : "",
   trailingSlash: true,
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
