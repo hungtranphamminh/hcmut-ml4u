3afec353517d6d72a5b86d263e2fb9dc885c6fdd
artemis_h <tranphamminhhung65@gmail.com>
2025-03-18 11:48:18 +0700

    feat: dockerized application for development with docker watch

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
     ],
     unoptimized: true,
   },
-  output: "export",
+  output: process.env.NODE_ENV === "production" ? "export" : "standalone",
   basePath: process.env.NODE_ENV === "production" ? "/hungtranghamminh.github.io" : "",
   assetPrefix: process.env.NODE_ENV === "production" ? "/hungtranghamminh.github.io" : "",
   trailingSlash: true,
